d878a35c3d96c768f6f9d7ed13dd5094ff9e44ac
Chandraparsad <chandraparsad02@gmail.com>
2023-10-11 00:26:39 +0530

    Views Changes

--- Currency/Karanc/views.py
+++ Currency/Karanc/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-    return render(request, 'Karanc/index.html')
+     return render(request, 'Karanc/index.html')
 
 def limit(request):
+    base_currency = request.GET.get('baseCurrency')
+    foreign_currency = request.GET.get('foreignCurrency')
+
+    # Do something with the variables in your view logic
+
+    return render(request, 'Karanc/limit.html')
     # if request.method=="POST":
-     return render(request, 'Karanc/limit.html')
 
 
