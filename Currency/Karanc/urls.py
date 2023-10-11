e6808a4bf8ba9712df66875d61a8e9fd8112f910
Chandraparsad <chandraparsad02@gmail.com>
2023-10-11 09:37:02 +0530

    Merge limit and input boxes

--- Currency/Karanc/urls.py
+++ Currency/Karanc/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name="index"),
-    path("limit/",views.limit,name="limit"),
+    path("",views.index,name="index")
 ]
--- Currency/Karanc/views.py
+++ Currency/Karanc/views.py
@@ -4,10 +4,6 @@ import requests
 def index(request):
      return render(request, 'Karanc/index.html')
 
-def limit(request):
-    base_currency = request.GET.get('baseCurrency')
-    foreign_currency = request.GET.get('foreignCurrency')
-    return redirect('another_function')
 
 
 
